caf1c254375debc98bfa278c56a5305542ddd818
jayswaldhun <jayswald@bu.edu>
2022-11-20 18:32:15 -0500

    Color class added

---
+++ src/game/utils/ConsoleColorsCodes.java
@@ -0,0 +1,17 @@
+//This class sets the color in the game
+public class ConsoleColorsCodes {
+
+    // Reset
+    public static final String RESET = "\033[0m";  // Reset text color
+    public static final String RED = "\033[0;31m";     // red color
+    public static final String GREEN = "\033[0;32m";   // green color
+
+    public static final String PURPLE = "\033[0;35m";  // purple color
+    public static final String CYAN = "\033[0;36m";    // cyan color
+    public static final String WHITE_BRIGHT = "\033[0;97m";  // white color
+    public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // green-bold color
+    public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// yellow bold color
+    public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";  // blue color
+
+}
+
